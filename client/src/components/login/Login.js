7c442553205f0098d4042112633b8b6231023ccc
Arsh Ahmad <152737810+rshdevmachine@users.noreply.github.com>
2025-10-04 01:49:26 +0530

    Update Login.js

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
     <div
       className="h-screen w-screen backdrop-blur-md flex justify-center"
       style={{
-        backgroundImage:`⁠url("/backgroundpic.jpeg")`,
+        backgroundImage: `url("https://images.unsplash.com/photo-1541339907198-e08756dedf3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         backgroundSize: "cover",
